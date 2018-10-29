a4074920c655a4ffd850b03917695a954bd61a86
Maxim Bilan <maximb.mail@gmail.com>
2018-10-29 13:40:37 +0200

    Added project template

---
+++ SwiftGoogleTranslate/SwiftGoogleTranslate/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftGoogleTranslate
+//
+//  Created by Maxim on 10/29/18.
+//  Copyright © 2018 Maksym Bilan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
