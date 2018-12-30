379aa4143d945f287f68e89423cbaaef1cbde1cf
Maxim Bilan <maximb.mail@gmail.com>
2018-12-30 11:21:58 +0200

    Created the framework target

---
+++ SwiftGoogleTranslate/SwiftGoogleTranslate/SwiftGoogleTranslate.h
@@ -0,0 +1,19 @@
+//
+//  SwiftGoogleTranslate.h
+//  SwiftGoogleTranslate
+//
+//  Created by Maxim on 12/30/18.
+//  Copyright © 2018 Maksym Bilan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftGoogleTranslate.
+FOUNDATION_EXPORT double SwiftGoogleTranslateVersionNumber;
+
+//! Project version string for SwiftGoogleTranslate.
+FOUNDATION_EXPORT const unsigned char SwiftGoogleTranslateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftGoogleTranslate/PublicHeader.h>
+
+
